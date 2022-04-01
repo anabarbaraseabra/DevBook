7906161e1b9b160bf0baef4a9156dee86aa2a663
anabarbaraseabra <anabarbaraseabra@gmail.com>
2022-03-31 22:59:43 -0300

    refactor in create user endpoint

--- api/src/controllers/users.go
+++ api/src/controllers/users.go
@@ -4,38 +4,46 @@ import (
 	"api/src/database"
 	"api/src/models"
 	"api/src/repository"
+	"api/src/responses"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
 )
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
 	requestBody, err := ioutil.ReadAll(r.Body)
 	if err != nil {
-		log.Fatal(err)
+		responses.Error(w, http.StatusUnprocessableEntity, err)
+		return
 	}
 
 	var user models.User
 	errUnmarshal := json.Unmarshal(requestBody, &user)
 	if errUnmarshal != nil {
-		log.Fatal(err)
+		responses.Error(w, http.StatusBadRequest, err)
+		return
+	}
+	if err = user.Prepare(); err != nil {
+		responses.Error(w, http.StatusBadRequest, err)
+		return
 	}
 
 	db, err := database.Connect()
 	if err != nil {
-		log.Fatal(err)
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
 	}
 	defer db.Close()
 
 	repository := repository.NewUserRepository(db)
 	userId, err := repository.Create(user)
 	if err != nil {
-		log.Fatal(err)
+		responses.Error(w, http.StatusInternalServerError, err)
+		return
 	}
 
-	w.Write([]byte(fmt.Sprintf("Inserted id: %s", userId)))
+	user.ID = userId
+	responses.JSON(w, http.StatusCreated, user)
 }
 
 func FindAllUsers(w http.ResponseWriter, r *http.Request) {
